d29b573a5146e145fe1325b8f8dfbdf33e4e5565
Tom Prats <tprats108@gmail.com>
2021-03-01 23:53:39 -0500

    Added support for content length, multipart byteranges, and invalid ranges

--- activestorage/app/controllers/active_storage/base_controller.rb
+++ activestorage/app/controllers/active_storage/base_controller.rb
@@ -10,12 +10,38 @@ class ActiveStorage::BaseController < ActionController::Base
 
   private
     def stream_chunk(blob)
-      range = Rack::Utils.get_byte_ranges(request.headers["HTTP_RANGE"], blob.byte_size).first
-      chunk = blob.download_chunk range
+      ranges = Rack::Utils.get_byte_ranges(request.headers["HTTP_RANGE"], blob.byte_size)
 
-      response.header["Content-Range"] = "bytes #{range.begin}-#{range.end}/#{blob.byte_size}"
-      response.status = 206
-      response.stream.write chunk
+      if ranges.all?(&:blank?)
+        response.status = 416
+      elsif ranges.length == 1
+        range = ranges.first
+        chunk = blob.download_chunk(range)
+
+        response.headers["Content-Length"] = chunk.length
+        response.headers["Content-Range"] = "bytes #{range.begin}-#{range.end}/#{blob.byte_size}"
+        response.status = 206
+        response.stream.write chunk
+      else
+        boundary = "3d6b6a416f9b5"
+        content_length = 0
+
+        response.headers["Content-Type"] = "Content-Type: multipart/byteranges; boundary=#{boundary}"
+        response.status = 206
+
+        ranges.compact.each do |range|
+          chunk = blob.download_chunk(range)
+          content_length += chunk.length
+
+          response.stream.write "--#{boundary}"
+          response.stream.write "Content-Type: #{blob.content_type_for_serving}"
+          response.stream.write "Content-Range: bytes #{range.begin}-#{range.end}/#{blob.byte_size}"
+          response.stream.write chunk
+        end
+
+        response.stream.write "--#{boundary}--"
+        response.headers["Content-Length"] = content_length
+      end
     ensure
       response.stream.close
     end
--- activestorage/app/controllers/concerns/active_storage/set_headers.rb
+++ activestorage/app/controllers/concerns/active_storage/set_headers.rb
@@ -7,6 +7,7 @@ module ActiveStorage::SetHeaders #:nodoc:
     def set_content_headers_from(blob)
       response.headers["Accept-Ranges"] = "bytes"
       response.headers["Content-Type"] = blob.content_type_for_serving
+      response.headers["Content-Length"] = blob.byte_size
       response.headers["Content-Disposition"] = ActionDispatch::Http::ContentDisposition.format \
         disposition: blob.forced_disposition_for_serving || params[:disposition] || "inline", filename: blob.filename.sanitized
     end
